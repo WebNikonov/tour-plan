c70a150590c46d71200ce0a0678e64d62eb056d2
WebNikonov <dmitrijnikonov076@gmail.com>
2023-05-24 14:05:50 +0300

    create: reviews slider

--- js/swiper.js
+++ js/swiper.js
@@ -1,9 +1,19 @@
-const swiper = new Swiper('.hotel-swiper', {
+const hotelSwiper = new Swiper('.hotel-swiper', {
     // Optional parameters
     loop: true,
     // Navigation arrows
     navigation: {
         nextEl: '.hotel-swiper__button--prev',
         prevEl: '.hotel-swiper__button--next',
+    },
+});
+
+const reviewsSwiper = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.reviews-slider__button--next',
+        prevEl: '.reviews-slider__button--prev',
     },
 });
